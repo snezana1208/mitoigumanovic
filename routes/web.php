c880e9f22ec5d70b1e0c07200ca5db6a2cc430f6
djolep87 <djolep987@gmail.com>
2022-09-25 13:32:19 +0200

    O skoli

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', 'PagesController@about');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
