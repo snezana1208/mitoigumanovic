a6fa1747cb74d62e057b1200d7a1e59d5cccd2e2
snezana1208 <snezagligo1208@gmail.com>
2022-10-26 18:18:18 +0200

    ђачки пар

--- resources/views/pages/za_djake/djacki_parlament.blade.php
+++ resources/views/pages/za_djake/djacki_parlament.blade.php
@@ -2,5 +2,47 @@
 @section('title', 'Ђачки парламент')
 
 @section('content')
-    
+<section class="tf-page-title">
+    <div class="overlay"></div>
+    <div class="overlay-bg"></div>
+    <img src="assets/images/background/img1innerpage.png" class="bg-inner1" alt="">
+    <img src="assets/images/background/img2innerpage.png" class="bg-inner2" alt="">
+    <img src="assets/images/background/img3innerpage.png" class="bg-inner3" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title inner">
+                    <h1 class="title">Ђачки парламент</h1>
+                    <div class="breadcrumbs">
+                        <ul class="jus-ct">
+                            <li><a href="/" class="f-rubik">Почетна</a></li>
+                            <li><p class="breadcrumbs-inner f-rubik">Ђачки парламент</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</section>
+<section class="tf-section tf-about">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-12">
+                <div class="sc-about-2 p-t19">
+                    <div class="title-heading">
+                        <p class="center f-mulish clr-pri-2  wow fadeIn animated" style="text-align: justify;" data-wow-delay="0.3ms" data-wow-duration="1000ms">
+                            Ђачки парламент је законом загарантована институција, која ученицима омогућава демократски начин удруживања ради заступања интереса свих ученика у школи, као и учешће ученика у доношењу одлука које се њих непосредно тичу. Ученички парламент, између осталог, даје мишљења и предлоге стручним органима школе, Школском одбору, Савету родитеља и директору о правилима понашања у школи, годишњем програму рада, школском развојном плану, слободним и ваннаставним активностима, учешћу на спортским и другим такмичењима и организацији свих манифестација ученика у школи и ван ње.
+
+Парламент чине по два представника из сваког одељења 7. и 8. разреда. Представнике може да изабере одељењска заједница, гласањем или на други начин. Представници одељења у Парламенту треба активно и одговорно да заступају ученичке предлоге, мишљења и жеље и пружају повратну информацију своме одељењу.
+                        </p>
+                    </div>
+
+                    <div class="inner">
+
+                    </div>
+                </div>
+            </div>
+        </div>  
+    </div>
+</section>
 @endsection
