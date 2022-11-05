ec60dc659aef2173d4eba9e3c63ba56b100d7d47
snezana1208 <snezagligo1208@gmail.com>
2022-11-05 12:48:58 +0100

    izmene

--- resources/views/pages/za_roditelje/otvorena_vrata.blade.php
+++ resources/views/pages/za_roditelje/otvorena_vrata.blade.php
@@ -1,6 +1,60 @@
 @extends('layouts.master')
-@section('title', 'Отворена врата')
+@section('title', 'Oтворена врата')
 
 @section('content')
-    
+<section class="tf-page-title">
+    <div class="overlay"></div>
+    <div class="overlay-bg"></div>
+    <img src="assets/images/background/img1innerpage.png" class="bg-inner1" alt="">
+    <img src="assets/images/background/img2innerpage.png" class="bg-inner2" alt="">
+    <img src="assets/images/background/img3innerpage.png" class="bg-inner3" alt="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title inner">
+                    <h1 class="title">Oтворена врата</h1>
+                    <div class="breadcrumbs">
+                        <ul class="jus-ct">
+                            <li><a href="/" class="f-rubik">Почетна</a></li>
+                            <li><p class="breadcrumbs-inner f-rubik">Oтворена врата</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</section>
+<section class="tf-section tf-about">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-12">
+                <div class="sc-about-2 p-t19">
+                    <div class="title-heading">
+                        <p class="center f-mulish clr-pri-2  wow fadeIn animated" style="text-align: justify;" data-wow-delay="0.3ms" data-wow-duration="1000ms">
+                            ДАНИ ОТВОРЕНИХ ВРАТА за школску 2022/2023. годину :
+                            <div class="img float-left mr-5 mt-5 mb-1">
+                                <div class="wrap-image ps-re">
+                                    <img src="assets/images/background/bg-aboutus-2.png" alt="" class="bg1-about">
+                                    <div class="image p-l70"><img src="assets/images/common/4a06cd2871d4e7af731712c115118867-0.jpg" alt="Images"></div>
+                                    <div class="image p-l70"><img src="assets/images/common/4a06cd2871d4e7af731712c115118867-1.jpg" alt="Images"></div>
+                                    
+                                </div>
+                            </div>
+                    
+                       
+
+
+
+          
+                        </p>
+                    </div>
+
+                    <div class="inner">
+
+                    </div>
+                </div>
+            </div>
+        </div>  
+    </div>
+</section>
 @endsection
